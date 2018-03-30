32b74b831183b7665dd06686845fa2f7f62f9cec
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-03-30 11:53:02 -0700

    replace default favicon

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -5,16 +5,27 @@ import { Project } from './models/project';
 @Injectable()
 export class ProjectService {
   projects: Project[] = [
-    { title: "Example Project 1",
+    {
+      title: "Example Project 1",
       artist: "Artist 1",
       medium: "Sculpture",
       imgURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/313e1541598569.57ac4240063b7.jpg",
-      info: "Project info..." },
-    { title: "Example Project 2",
+      info: "Project info..."
+    },
+    {
+      title: "Example Project 2",
       artist: "Artist 2",
       medium: "Digital Art",
       imgURL: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/78a53a52241083.59ea1e65e1f07.jpg",
-      info: "Project info..." }
+      info: "Project info..."
+    },
+    {
+      title: "Example Music Project",
+      artist: "Musician 1",
+      medium: "Electronic Music",
+      imgURL: "https://i1.sndcdn.com/artworks-000312169044-um407k-t500x500.jpg",
+      info: "Project info..."
+    }
   ];
 
   constructor() { }
