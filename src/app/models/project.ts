cfaa41c342c978c72e74511a14a7169841298c65
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-03-30 11:05:29 -0700

    add images to project model and card

--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -1,3 +1,4 @@
 export class Project {
   title: string;
+  imgURL: string;
 }
--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -5,8 +5,8 @@ import { Project } from './models/project';
 @Injectable()
 export class ProjectService {
   projects: Project[] = [
-    { title: "Example Project 1" },
-    { title: "Example Project 2" }
+    { title: "Example Project 1", imgURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/313e1541598569.57ac4240063b7.jpg" },
+    { title: "Example Project 2", imgURL: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/78a53a52241083.59ea1e65e1f07.jpg" }
   ];
 
   constructor() { }
